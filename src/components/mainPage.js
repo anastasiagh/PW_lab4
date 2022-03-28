13c5a37252f1ca0af2446b1f00bde58d8f05d392
anastasiagh <gheorghita.anastasia@isa.utm.md>
2022-03-28 23:06:38 +0300

    added main page component

---
+++ src/components/mainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Main = () => {
+        return (
+            <div className="main">
+                {/* <p>Hello</p> */}
+                <input type="button" className="button-try-now" value="Try now" />
+            </div>
+        )
+}
+
+export default Main;
