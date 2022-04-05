68977176306a50f865b2ace972a7db8850eaefef
anastasiagh <gheorghita.anastasia@isa.utm.md>
2022-04-05 19:29:07 +0300

    modified Main Page, added quiz boxes

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -4,8 +4,21 @@ import { NavLink } from 'react-router-dom';
 const Main = () => {
         return (
             <div className="main">
-                {/* <p>Hello</p> */}
-                <input type="button" className="button-try-now" value="Try now" />
+                    <div className="name">
+                        <h1>Hey you, choose a quiz</h1>
+                    </div>
+                    <div className="quizes-here">
+                        <div className='quiz'>
+                            <h2>Quiz 1</h2>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            <input type="button" className="start-quiz" value="Start quiz" />
+                        </div>
+                        <div className='quiz'>
+                            <h2>Quiz 1</h2>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            <input type="button" className="start-quiz" value="Start quiz" />
+                        </div>
+                    </div>
             </div>
         )
 }
